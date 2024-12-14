d8a44fa38f48dea9878b7305fa7e7afbd9e25062
zenzen-git <190453781+zenzen-git@users.noreply.github.com>
2024-12-14 17:14:48 +0800

    Fix issue regarding the vercel deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
+
 export default {
     darkMode: ["class"],
     content: ["./index.html", "./src/**/*.{ts,tsx}"],
@@ -56,6 +58,6 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 }
 
